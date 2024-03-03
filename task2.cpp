555256b18273de9113ed458b7c36155004cab011
oksanaemelyanova <159081930+oksanaemelyanova@users.noreply.github.com>
2024-03-03 22:29:34 +0000

    errors corrected

--- task2.cpp
+++ task2.cpp
@@ -6,23 +6,33 @@ struct bank_account {
   int account_balance;
 };
 
-void create_account(bank_account *account) {
-  std::cout << "enter account number: \n";
-  std::cin >> account->account_number;
-  std::cout << "enter account holder name: \n";
-  std::cin >> account->account_holder_name;
-  std::cout << "enter account balance: \n";
-  std::cin >> account->account_balance;
-  std::cout << "enter new balance: \n";
-  std::cin >> account->account_balance;
-  std::cout << "your new account is: " << account->account_holder_name << ", "
-            << account->account_number << ", " << account->account_balance
-            << std::endl;
+void change_balance(bank_account *account, int new_balance) {
+  
+  account->account_balance = new_balance;
+  
 }
 
 int task2() {  
   bank_account account1;
+  int new_balance;
 
-  create_account(&account1);
+  std::cout << "enter account number: \n";
+  std::cin >> account1.account_number;
+  std::cout << "enter account holder name: \n";
+  std::cin >> account1.account_holder_name;
+  std::cout << "enter account balance: \n";
+  std::cin >> account1.account_balance;
+
+
+
+  std::cout << "enter new balance: \n";
+  std::cin >> new_balance;
+
+  change_balance(&account1, new_balance);
+
+  std::cout << "your account: " << account1.account_holder_name << ", "
+            << account1.account_number << ", " << account1.account_balance
+            << std::endl;
+
   return (0);
 }
